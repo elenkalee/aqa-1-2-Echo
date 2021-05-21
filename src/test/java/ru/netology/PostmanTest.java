493fa55b452121f120cabe92aa888ea528b4afcf
elenkalee <elena.lipukhina@gmail.com>
2021-05-21 16:49:45 +0300

    failed tast

--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -15,7 +15,7 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 }
